676f80f9d66cff42ec72aa633a33ba7607a2444c
Andrii Yarosh <yarosh.andrey2001@gmail.com>
2020-05-21 01:39:30 +0300

    Created Station migration/model/controller

---
+++ database/migrations/2020_05_20_222627_create_stations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('city_id', false, 10);
+            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
+            $table->boolean('isOpen');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stations');
+    }
+}
